62d08e19d6afc2fd557571e306f45093c33549bb
Dinesh Shaw <idineshshaw7@gmail.com>
2023-04-07 18:16:36 +0530

    updated theme

--- pages/_document.js
+++ pages/_document.js
@@ -33,7 +33,7 @@ class MyDocument extends Document {
           />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#2b5797" />
-          <meta name="theme-color" content="#f5f1eb" />
+          <meta name="theme-color" content="#ececec" />
           <meta name="description" content="Dinesh Shaw" />
           <meta name="msapplication-TileColor" content="#3291ff"></meta>
           <link rel="manifest" href="/manifest.json" />
