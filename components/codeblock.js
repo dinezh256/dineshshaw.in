a359cd5a3f70f62e763db2c795d8e34df80bac3e
Dinesh Shaw <idineshshaw7@gmail.com>
2023-06-17 00:29:21 +0530

    change code block style

--- components/codeblock.js
+++ components/codeblock.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
-import blockStyle from "react-syntax-highlighter/dist/cjs/styles/prism/coldark-dark";
+import blockStyle from "react-syntax-highlighter/dist/cjs/styles/prism/solarizedlight";
 
 const CodeBlock = ({ language, value }) => {
   return (
