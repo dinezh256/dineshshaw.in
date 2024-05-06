b8eee70bba1278c249e7e2a4648b7f516e61a277
Dinesh Shaw <idineshshaw7@gmail.com>
2024-05-06 19:03:41 +0530

    disable dark reader from mutating color schemes

--- pages/_document.js
+++ pages/_document.js
@@ -41,9 +41,9 @@ class MyDocument extends Document {
             href="/favicon-16x16.png"
           />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="manifest" href="/manifest.json" />
           <meta name="msapplication-TileColor" content="#2b5797" />
           <meta name="theme-color" content="#f8f8f8" />
-          <link rel="manifest" href="/manifest.json" />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://dineshshaw.in" />
           <meta property="og:title" content="Dinesh Shaw" />
@@ -58,8 +58,9 @@ class MyDocument extends Document {
           <meta property="og:image:type" content="image/png" />
           <meta property="og:image:width" content="512" />
           <meta property="og:image:height" content="512" />
-          <meta property="og:site_name" content="Dinesh Shaw"></meta>
-          <meta property="og:locale" content="en-US"></meta>
+          <meta property="og:site_name" content="Dinesh Shaw" />
+          <meta property="og:locale" content="en-US" />
+          <meta name="darkreader-lock" />
         </Head>
         <body>
           <Main />
