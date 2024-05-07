5fff208818ce66403ba3b708c06c2fb470650455
Dinesh Shaw <idineshshaw7@gmail.com>
2024-05-07 17:53:13 +0530

    minor changes

--- components/nameCard.js
+++ components/nameCard.js
@@ -27,7 +27,7 @@ const NameCard = () => {
         <div className="name-card-meta">
           <h1>Dinesh Shaw</h1>
           <h2>Software Engineer</h2>
-          <span>Jamshedpur, Jharkhand 🇮🇳</span>
+          <span>Jamshedpur, JH  |  🇮🇳</span>
         </div>
       </div>
     </div>
