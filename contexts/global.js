e5cf82f64169e14a36d9d3f2304616c39064a264
Dinesh Shaw <idineshshaw7@gmail.com>
2024-05-17 22:27:25 +0530

    loader fix

--- contexts/global.js
+++ contexts/global.js
@@ -3,7 +3,7 @@ import { useState, createContext } from "react";
 export const GlobalContext = createContext();
 
 export const GlobalContextProvider = ({ children }) => {
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
 
     return (
         <GlobalContext.Provider value={{ isLoading, setIsLoading }}>
